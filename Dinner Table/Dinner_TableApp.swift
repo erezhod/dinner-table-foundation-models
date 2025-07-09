ad16e8bb1ca1990b7a2aa9026effb8ae9a421ef7
Erez Hod <erez@erezhod.com>
2025-07-09 15:29:40 +0300

    Initial Commit

---
+++ Dinner Table/Dinner_TableApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dinner_TableApp.swift
+//  Dinner Table
+//
+//  Created by Erez Hod on 9/7/25.
+//
+
+import SwiftUI
+
+@main
+struct Dinner_TableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
